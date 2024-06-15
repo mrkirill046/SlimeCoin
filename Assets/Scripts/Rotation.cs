7f274d6c41e2d5c9c2b21d1d034155e3419ccb34
Kirill <mrkirill046@yandex.ru>
2024-06-15 22:42:36 +0300

    2-commitfix

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;
+    
+    private void LateUpdate()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
